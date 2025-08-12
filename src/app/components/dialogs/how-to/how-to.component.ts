ec8e84a062de102e0338e442c56521b6e01a1826
ChristianCaldwell55 <christian.c2969@gmail.com>
2025-08-11 21:53:29 -0500

    Update card style, refactor difficulty in how to dialog

--- src/app/components/dialogs/how-to/how-to.component.ts
+++ src/app/components/dialogs/how-to/how-to.component.ts
@@ -3,10 +3,11 @@ import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-how-to',
-  imports: [CommonModule, MatButtonModule, MatExpansionModule],
+  imports: [CommonModule, MatButtonModule, MatExpansionModule, MatIconModule],
   templateUrl: './how-to.component.html',
   styleUrls: ['./how-to.component.scss']
 })
