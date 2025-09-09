7875c1260487163cb07b7eecb98152c17abd4021
ChristianCaldwell55 <christian.c2969@gmail.com>
2025-09-08 22:00:38 -0500

    First split of components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { GameComponent } from './pages/game/game.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { PairsComponent } from './pages/pairs/pairs.component';
+import { SolutionComponent } from './pages/solution/solution.component';
 
 export const routes: Routes = [
-    { path: '', component: GameComponent },
+    { path: 'legacy', component: GameComponent },
+    { path: '', component: MenuComponent },
+    { path: 'pairs', component: PairsComponent },
+    { path: 'solution', component: SolutionComponent },
+    { path: '**', redirectTo: '' }
 ];
--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -2,11 +2,16 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { GameService } from '../../services/game.service';
+import { AnalyticsService } from '../../services/analytics.service';
+import { HowToComponent } from '../../components/dialogs/how-to/how-to.component';
+import { GameModes, GameDifficulties } from '../../model/enum/game.enums';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -16,5 +21,45 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
+
+  // form values
+  selectedMode: string = '';
+  selectedDifficulty: string = '';
+
+  // element references
+  dialogRef: any;
+
+  constructor(
+    private gameService: GameService,
+    private analyticsService: AnalyticsService,
+    private dialog: MatDialog,
+    private router: Router
+  ) {}
+
+  startGame() {
+    // set game settings to default if not set by player
+    this.selectedMode = this.selectedMode == '' ? GameModes.PAIRS : this.selectedMode;
+    this.selectedDifficulty = this.selectedDifficulty == '' ? GameDifficulties.MEDIUM : this.selectedDifficulty;
+    // update game service to match desired game settings
+    this.gameService.setSelectedMode(this.selectedMode as GameModes);
+    this.gameService.setDifficulty(this.selectedDifficulty as GameDifficulties);
+    // setup the game with settings
+    this.gameService.setGameSettings(this.gameService.getSelectedMode(), this.gameService.getDifficulty());
+    // track game start event with selected settings
+    this.analyticsService.trackGameModeStart(this.gameService.getSelectedMode(), { difficulty: this.selectedDifficulty as GameDifficulties });
+    // route to the selected game
+    this.router.navigate([this.gameService.getSelectedMode()]);
+  }
+
+  howToClicked() {
+    this.dialogRef = this.dialog.open(HowToComponent, {
+      height: 'auto',
+      maxHeight: '90vh',
+      width: '90%',
+      maxWidth: '700px',
+      disableClose: false,
+    });
+    this.analyticsService.howToPlayEvent();
+  }
 
 }
